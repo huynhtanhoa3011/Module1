07c7eec32dbf24d39e39ffd58d3aba059e3ce204
huynhtanhoa3011 <85146749+huynhtanhoa3011@users.noreply.github.com>
2021-06-30 16:46:53 +0700

    Add files via upload

---
+++ lesson 6/rgb.js
@@ -0,0 +1,11 @@
+function changeColor() {
+    var red = document.getElementById("red").value;
+    var green = document.getElementById("green").value;
+    var blue = document.getElementById("blue").value;
+    document.getElementById("test").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+    document.getElementById("r").innerHTML = red;
+    document.getElementById("g").innerHTML = green;
+    document.getElementById("b").innerHTML = blue;
+
+
+}
